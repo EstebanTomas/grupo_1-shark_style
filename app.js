d204d67697251a01fca77a1be18e8803e9f0ba4d
EstebanTomas <tomasvaliente86@gmail.com>
2021-07-19 19:46:43 -0300

    actualizo main

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+const publicpath = path.join(__dirname, './public');
+
+app.use(express.static(publicpath));
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto 3000');
+});
+
+app.get('/register',(req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/register.html'));
+});
+
+app.get('/home',(req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/index.html'));
+});
