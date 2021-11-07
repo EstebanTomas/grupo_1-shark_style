3760bcd96172358f650faedd06b42831c3e5aab1
AgusLarralde <dosagus66@gmail.com>
2021-11-07 20:31:14 -0300

    Cargo los datos de prueba para los productos en el SCRIPT de la Base de Datos

--- sharkstyle.sql
+++ sharkstyle.sql
@@ -16,6 +16,69 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Dumping routines for database 'sharkstyle'
+--
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `sharkstyle` /*!40100 DEFAULT 
+CHARACTER SET utf8 */;
+USE `sharkstyle`;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `products` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `description` text,
+  `price` mediumint(8) unsigned NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `category` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'Camisa','Elaborada con la tela mas fina de importaciÃ³n',23000,'Hombre','Camisas'),(2,'Camisa negra','Elaborada con la tela mas fina de importaciÃ³n',23000,'Hombre','Camisas'),(3,'Camisa blanca','Elaborada con la tela mas fina de importaciÃ³n',23000,'Hombre','Camisas'),(4,'Camisa celeste','Elaborada con la tela mas fina de importaciÃ³n',23000,'Hombre','Camisas');
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL,
+  `lastname` varchar(60) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `role` tinyint(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `images`
 --
 
@@ -24,7 +87,7 @@ DROP TABLE IF EXISTS `images`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `images` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `img` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
+  `img` varchar(200) NOT NULL,
   `product_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `product_images_id_idx` (`product_id`),
@@ -38,6 +101,7 @@ CREATE TABLE `images` (
 
 LOCK TABLES `images` WRITE;
 /*!40000 ALTER TABLE `images` DISABLE KEYS */;
+INSERT INTO `images` VALUES (1,'t-shirt.orange.jpg',1),(2,'t-shirt.black.jpg',2),(3,'t-shirt.white.jpg',3),(4,'t-shirt.skyblue.jpg',4),(5,'p1.jpg',1),(6,'p2.jpg',1);
 /*!40000 ALTER TABLE `images` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -50,8 +114,8 @@ DROP TABLE IF EXISTS `models`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `models` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `img` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
-  `colors` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `img` varchar(200) NOT NULL,
+  `colors` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -62,34 +126,8 @@ CREATE TABLE `models` (
 
 LOCK TABLES `models` WRITE;
 /*!40000 ALTER TABLE `models` DISABLE KEYS */;
+INSERT INTO `models` VALUES (1,'t-shirt.black.jpg','negro'),(2,'t-shirt.skyblue.jpg','celeste'),(3,'t-shirt.white.jpg','blanco'),(4,'t-shirt.orange.jpg','naranja');
 /*!40000 ALTER TABLE `models` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `orders`
---
-
-DROP TABLE IF EXISTS `orders`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `orders` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `order_date` date NOT NULL,
-  `date_of_delivery` date NOT NULL,
-  `user_id` int(10) unsigned NOT NULL COMMENT 'table to record customer orders',
-  PRIMARY KEY (`id`),
-  KEY `user_id_idx` (`user_id`),
-  CONSTRAINT `user_id_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `orders`
---
-
-LOCK TABLES `orders` WRITE;
-/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -117,7 +155,66 @@ CREATE TABLE `product_model` (
 
 LOCK TABLES `product_model` WRITE;
 /*!40000 ALTER TABLE `product_model` DISABLE KEYS */;
+INSERT INTO `product_model` VALUES (1,1,1),(2,1,2),(3,1,3),(4,2,2),(5,2,3),(6,2,3),(7,2,4),(8,3,1),(9,3,2),(10,3,2),(11,3,4),(12,4,1),(13,4,3),(14,4,4);
 /*!40000 ALTER TABLE `product_model` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sizes`
+--
+
+DROP TABLE IF EXISTS `sizes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `sizes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` int(10) unsigned NOT NULL,
+  `xs` tinyint(1) DEFAULT '0',
+  `s` tinyint(1) DEFAULT '0',
+  `m` tinyint(1) DEFAULT '0',
+  `l` tinyint(1) DEFAULT '0',
+  `xl` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `product_id_idx` (`product_id`),
+  CONSTRAINT `product_id_3` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sizes`
+--
+
+LOCK TABLES `sizes` WRITE;
+/*!40000 ALTER TABLE `sizes` DISABLE KEYS */;
+INSERT INTO `sizes` VALUES (1,1,1,1,1,1,1),(2,2,1,0,1,0,1),(3,3,0,0,1,0,0),(4,4,0,0,1,1,0);
+/*!40000 ALTER TABLE `sizes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `orders` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `order_date` date NOT NULL,
+  `date_of_delivery` date NOT NULL,
+  `user_id` int(10) unsigned NOT NULL COMMENT 'table to record customer orders',
+  PRIMARY KEY (`id`),
+  KEY `user_id_idx` (`user_id`),
+  CONSTRAINT `user_id_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -176,33 +273,6 @@ LOCK TABLES `product_shop` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `products`
---
-
-DROP TABLE IF EXISTS `products`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `products` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
-  `description` text COLLATE utf8mb4_general_ci,
-  `price` mediumint(8) unsigned NOT NULL,
-  `gender` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `category` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `products`
---
-
-LOCK TABLES `products` WRITE;
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `shopping`
 --
 
@@ -217,8 +287,8 @@ CREATE TABLE `shopping` (
   PRIMARY KEY (`id`),
   KEY `product_shop_id_idx` (`product_shop_id`),
   KEY `user_id_idx` (`user_id`),
-  CONSTRAINT `product_shop_id` FOREIGN KEY (`product_shop_id`) REFERENCES `product_shop` (`id`),
-  CONSTRAINT `user_id_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+  CONSTRAINT `user_id_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `product_shop_id` FOREIGN KEY (`product_shop_id`) REFERENCES `product_shop` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -232,36 +302,6 @@ LOCK TABLES `shopping` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `sizes`
---
-
-DROP TABLE IF EXISTS `sizes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `sizes` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `product_id` int(10) unsigned NOT NULL,
-  `xs` tinyint(1) DEFAULT '0',
-  `s` tinyint(1) DEFAULT '0',
-  `m` tinyint(1) DEFAULT '0',
-  `l` tinyint(1) DEFAULT '0',
-  `xl` tinyint(1) DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `product_id_idx` (`product_id`),
-  CONSTRAINT `product_id_3` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `sizes`
---
-
-LOCK TABLES `sizes` WRITE;
-/*!40000 ALTER TABLE `sizes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `sizes` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `user_img`
 --
 
@@ -270,7 +310,7 @@ DROP TABLE IF EXISTS `user_img`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `user_img` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `avatar` varchar(200) COLLATE utf8mb4_general_ci DEFAULT 'user_anonimo.jpg',
+  `avatar` varchar(200) DEFAULT 'user_anonimo.jpg',
   `user_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id_idx` (`user_id`),
@@ -287,37 +327,6 @@ LOCK TABLES `user_img` WRITE;
 /*!40000 ALTER TABLE `user_img` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(60) COLLATE utf8mb4_general_ci NOT NULL,
-  `lastname` varchar(60) COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(60) COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `role` tinyint(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping routines for database 'sharkstyle'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -328,4 +337,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-22 17:35:11
+-- Dump completed on 2021-10-19 18:14:25
