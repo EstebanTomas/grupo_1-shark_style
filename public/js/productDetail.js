55d09bec447d4943a97a2b9a0e8d412bb29da795
AgusLarralde <dosagus66@gmail.com>
2021-11-20 01:33:12 -0300

    Guardando cambios

--- public/js/productDetail.js
+++ public/js/productDetail.js
@@ -1,4 +1,11 @@
 window.onload = function () {
     let image = document.getElementById("imagen-principal");
     let p1 = document.getElementById("p3");
+
+    function copy() {
+        var copyText = document.querySelector("#input");
+        copyText.select();
+        document.execCommand("copy");
+    }
+    document.querySelector("#copy").addEventListener("click", copy);
 }
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,7 +5,8 @@ const path = require("path")
 const adminControllers = require("../controllers/adminControllers");
 
 const Upload = require("../../middlewares/saveImgUserMidlewares");
-const ValidationsOfRegister = require("../../middlewares/validationsOfUser")
+const ValidationsOfRegister = require("../../middlewares/validationsOfUser");
+const UploadProducts = require("../../middlewares/saveImgProductsMiddlewares");
 
 // **USERS**
 router.post("/register", Upload.single('avatar') , ValidationsOfRegister ,adminControllers.saveRegister);
@@ -15,5 +16,14 @@ router.post("/edit/:id", Upload.single("avatarEdit"), ValidationsOfRegister, adm
 
 // **PRODUCTS**
 router.get('/products', adminControllers.adminProducts)
+// route of create
+router.get('/products/create', adminControllers.productCreate);
+router.post('/products/create', UploadProducts.array("img", 3), adminControllers.create);
+// this path shows the edit form
+router.get('/products/edit/:id', adminControllers.editProduct);
+router.put('/products/edit/:id', UploadProducts.array("img", 3), adminControllers.edit);
+// routes in delete
+router.delete('/products/delete/:id', adminControllers.delete);
+
 
 module.exports = router;
