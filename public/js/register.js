085e86c3dd1a31f42b25b48dc0d0746f68c60f79
EstebanTomas <tomasvaliente86@gmail.com>
2021-11-28 17:45:40 -0300

    trabaja en las validaciones del login y en el header

--- public/js/register.js
+++ public/js/register.js
@@ -1,11 +1,36 @@
 window.onload = function () {
-    let form = document.querySelector("form.main-form");
+    /* COUNTRYS */
+    let form = document.querySelector("form#form");
     let countryName = document.querySelector("#name");
     let countryLastName = document.querySelector("#lastName");
     let countryEmail = document.querySelector("#email");
     let countryPassword = document.querySelector("#password");
-    /* Descripción
-Editar
+    /* msg of h4 */
+    let nameH4 = document.querySelector("h4.name");
+    let lastNameH4 = document.querySelector("h4.lastName");
+    let emailH4 = document.querySelector("h4.email");
+    let passwordH4 = document.querySelector("h4.password");
+
+    form.addEventListener("submit", (e) => {
+       
+        if (countryName.value.length < 2 ) {
+            nameH4.innerHTML = "Tu nombre debe ser mayor a 2 caracteres";
+        }
+        if (countryName.value == "") {
+            e.preventDefault();
+            nameH4.innerHTML = "Debes escribir un nombre de usuario"
+        }
+        if (countryLastName.value.length < 4 ) {
+            lastNameH4.innerHTML = "Tu apellido debe ser mayor a 4 caracteres";
+        }
+        if (countryLastName.value == "") {
+            e.preventDefault();
+            lastNameH4.innerHTML = "Debes escribir un nombre de usuario"
+        }
+    });
+    /* 
+             Descripción
+            Editar
 ○ Nombre y apellido
 
 ■ Obligatorio.
@@ -22,22 +47,6 @@ Editar
 ■ Obligatoria.
 ■ Deberá tener al menos 8 caracteres.
 ■ (Opcional) → Deberá tener letras mayúsculas, minúsculas, un
-número y un carácter especial. */
-    countryName.addEventListener("mouseover", () => {
-
-    })
-    countryLastName.addEventListener("")
-
-    form.addEventListener("submit", (event) => {
-        if (countryName.value == "") {
-            event.preventDefault();
-        } else if (countryName.value.length < 2) {
-            event.preventDefault();
-        } else if (countryLastName.value == "") {
-            event.preventDefault();
-        } else if (countryLastName.value.length < 2) {
-            event.preventDefault();
-        }
-    });
+              número y un carácter especial. */
 
 }
